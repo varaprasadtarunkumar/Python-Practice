0c95d3191dda29d0d1067495d093ea61c4b7e7db
G Srinivasa Eswara Vara Prasad <105410227+varaprasadtarunkumar@users.noreply.github.com>
2023-02-02 18:49:19 +0530

    Create Vol_Sphere&Dist_btw_Pts.py

---
+++ Vol_Sphere&Dist_btw_Pts.py
@@ -0,0 +1,11 @@
+r=int(input("Enter the radius"))
+vol=(4/3)*(3.14)**3
+print(vol)
+
+
+x1=int(input("Enter x1 "))
+x2=int(input("Enter x2 "))
+y1=int(input("Enter y1 "))
+y2=int(input("Enter y2 "))
+dist=((x1-x2)**2+(x3-x4)**2)**0.5
+print(dist)
