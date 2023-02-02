141ebf9e92df8dd96dfcbfbe6cf220216914daa0
G Srinivasa Eswara Vara Prasad <105410227+varaprasadtarunkumar@users.noreply.github.com>
2023-02-02 18:55:06 +0530

    Update CountDown(reverse).py

--- CountDown(reverse).py
+++ CountDown(reverse).py
@@ -1,3 +1,9 @@
 a=int(input("Enter the number"))
 for i in range(a,0,-1): # -1 since we are decrementing the value from a to 0 by 1
   print(i,end=" ")
+
+  # Using While Loop
+a=int(input("Enter the number"))
+while a>=0:
+    print(a,end=" ")
+    a-=1
